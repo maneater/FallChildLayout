24f819ab18f5b89c097abb0eb93d56641fc46e9e
liang <shanghliang@163.com>
2016-10-27 18:06:07 +0800

    update

--- app/src/main/java/com/maneater/android/view/fallchildlayout/fall/AnimateView.java
+++ app/src/main/java/com/maneater/android/view/fallchildlayout/fall/AnimateView.java
@@ -1,4 +1,4 @@
-package com.maneater.android.view.fallchildlayout;
+package com.maneater.android.view.fallchildlayout.fall;
 
 import android.animation.Animator;
 import android.animation.ObjectAnimator;
@@ -15,6 +15,8 @@ import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.animation.LinearInterpolator;
+
+import com.maneater.android.view.fallchildlayout.R;
 
 import java.lang.ref.WeakReference;
 import java.util.ArrayList;
@@ -329,7 +331,7 @@ public class AnimateView extends View implements AnimateView.ChildListener {
         return super.onTouchEvent(event);
     }
 
-    public static interface AnimateChild {
+    public interface AnimateChild {
 
         boolean onTouchDown();
 
@@ -376,134 +378,11 @@ public class AnimateView extends View implements AnimateView.ChildListener {
         void setListener(ChildListener childListener);
     }
 
-    interface ChildListener {
+    public interface ChildListener {
         void onClick(AnimateChild animateChild);
     }
 
     public static abstract class BaseChild implements AnimateChild {
-
-
-        float transX = 0;
-        float transY = 0;
-        float scaleX = 1;
-        float scaleY = 1;
-        float rotation = 0;
-
-        float left = 0;
-        float top = 0;
-
-        private ChildListener childListener = null;
-
-
-        @Override
-        public void setListener(ChildListener childListener) {
-            this.childListener = childListener;
-        }
-
-        @Override
-        public boolean onTouchDown() {
-            if (childListener != null) {
-                childListener.onClick(this);
-                return true;
-            }
-            return false;
-        }
-
-        @Override
-        public void setFrame(float left, float top) {
-            this.left = left;
-            this.top = top;
-        }
-
-        @Override
-        public float getX() {
-            return left + getTransX();
-        }
-
-        @Override
-        public float getY() {
-            return top + getTransY();
-        }
-
-        @Override
-        public AnimateChild setX(float x) {
-            setTransX(x - left);
-            return this;
-        }
-
-        @Override
-        public AnimateChild setY(float y) {
-            setTransY(y - top);
-            return this;
-        }
-
-        @Override
-        public float getLeft() {
-            return this.left + getTransX();
-        }
-
-        @Override
-        public float getTop() {
-            return this.top + getTransY();
-        }
-
-        @Override
-        public float getTransX() {
-            return transX;
-        }
-
-        @Override
-        public float getTransY() {
-            return transY;
-        }
-
-        @Override
-        public float getScaleX() {
-            return scaleX;
-        }
-
-        @Override
-        public float getScaleY() {
-            return scaleY;
-        }
-
-        @Override
-        public float getRotation() {
-            return rotation;
-        }
-
-        @Override
-        public AnimateChild setTransX(float transX) {
-            this.transX = transX;
-            return this;
-        }
-
-        @Override
-        public AnimateChild setTransY(float transY) {
-            this.transY = transY;
-            return this;
-        }
-
-
-        @Override
-        public AnimateChild setTrans(float transX, float transY) {
-            this.transX = transX;
-            this.transY = transY;
-            return this;
-        }
-
-        @Override
-        public AnimateChild setScale(float scaleX, float scaleY) {
-            this.scaleX = scaleX;
-            this.scaleY = scaleY;
-            return this;
-        }
-
-        @Override
-        public AnimateChild setRotation(float rotation) {
-            this.rotation = rotation;
-            return this;
-        }
 
     }
 
---
+++ app/src/main/java/com/maneater/android/view/fallchildlayout/fall/BaseChild.java
@@ -0,0 +1,131 @@
+package com.maneater.android.view.fallchildlayout.fall;
+
+/**
+ * Created by Administrator on 2016/10/27 0027.
+ */
+
+public abstract class BaseChild implements AnimateView.AnimateChild {
+
+
+    float transX = 0;
+    float transY = 0;
+    float scaleX = 1;
+    float scaleY = 1;
+    float rotation = 0;
+
+    float left = 0;
+    float top = 0;
+
+    private AnimateView.ChildListener childListener = null;
+
+
+    @Override
+    public void setListener(AnimateView.ChildListener childListener) {
+        this.childListener = childListener;
+    }
+
+    @Override
+    public boolean onTouchDown() {
+        if (childListener != null) {
+            childListener.onClick(this);
+            return true;
+        }
+        return false;
+    }
+
+    @Override
+    public void setFrame(float left, float top) {
+        this.left = left;
+        this.top = top;
+    }
+
+    @Override
+    public float getX() {
+        return left + getTransX();
+    }
+
+    @Override
+    public float getY() {
+        return top + getTransY();
+    }
+
+    @Override
+    public AnimateView.AnimateChild setX(float x) {
+        setTransX(x - left);
+        return this;
+    }
+
+    @Override
+    public AnimateView.AnimateChild setY(float y) {
+        setTransY(y - top);
+        return this;
+    }
+
+    @Override
+    public float getLeft() {
+        return this.left + getTransX();
+    }
+
+    @Override
+    public float getTop() {
+        return this.top + getTransY();
+    }
+
+    @Override
+    public float getTransX() {
+        return transX;
+    }
+
+    @Override
+    public float getTransY() {
+        return transY;
+    }
+
+    @Override
+    public float getScaleX() {
+        return scaleX;
+    }
+
+    @Override
+    public float getScaleY() {
+        return scaleY;
+    }
+
+    @Override
+    public float getRotation() {
+        return rotation;
+    }
+
+    @Override
+    public AnimateView.AnimateChild setTransX(float transX) {
+        this.transX = transX;
+        return this;
+    }
+
+    @Override
+    public AnimateView.AnimateChild setTransY(float transY) {
+        this.transY = transY;
+        return this;
+    }
+
+
+    @Override
+    public AnimateView.AnimateChild setTrans(float transX, float transY) {
+        this.transX = transX;
+        this.transY = transY;
+        return this;
+    }
+
+    @Override
+    public AnimateView.AnimateChild setScale(float scaleX, float scaleY) {
+        this.scaleX = scaleX;
+        this.scaleY = scaleY;
+        return this;
+    }
+
+    @Override
+    public AnimateView.AnimateChild setRotation(float rotation) {
+        this.rotation = rotation;
+        return this;
+    }
+}
